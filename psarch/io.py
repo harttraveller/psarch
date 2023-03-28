36d5fc4946ea9b5812343630b7f01347d0faf250
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-28 01:58:47 -0400

    psarch/io: refactor

--- psarch/io.py
+++ psarch/io.py
@@ -2,19 +2,23 @@ from zstandard import ZstdDecompressor
 
 
 class ZSTIO:
-    def __init__(self, file: str, chunk_size: int = 16384) -> None:
-        self.fh = open(file, "rb")
+    "source: https://github.com/pushshift/zreader"
+
+    def __init__(
+        self, file: str, chunk_size: int = int(2**10), max_window_size=int(2**31)
+    ) -> None:
+        self.decompressor = ZstdDecompressor(
+            max_window_size=max_window_size
+        ).stream_reader(open(file, "rb"))
         self.chunk_size = chunk_size
-        self.dctx = ZstdDecompressor(max_window_size=int(2**31))
-        self.reader = self.dctx.stream_reader(self.fh)
         self.buffer = ""
 
     def readlines(self):
         while True:
-            chunk = self.reader.read(self.chunk_size).decode()
+            chunk = self.decompressor.read(self.chunk_size).decode()
             if not chunk:
                 break
             lines = (self.buffer + chunk).split("\n")
+            self.buffer = lines[-1]
             for line in lines[:-1]:
                 yield line
-            self.buffer = lines[-1]
