bb1cf714a475e821273a1ccc32c5c01f0a924df3
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-28 18:41:55 -0400

    update

--- psarch/io.py
+++ psarch/io.py
@@ -1,43 +1,54 @@
 from __future__ import annotations
 
 import orjson
-from pathlib import Path
+from tqdm import tqdm
+from typing import Callable, Any
 from zstandard import ZstdDecompressor
 
 
 class ZSTJSONL:
-    def __init__(
-        self, path: str, chunk_size: int = int(2**20), max_window_size=int(2**31)
-    ) -> None:
-        # ! bug (hypothesis): if chunk size is less than the length of a line, bug introduced
-        # ! and cannot read
-        self.reader = ZstdDecompressor(max_window_size=max_window_size).stream_reader(
+    def __init__(self, path: str, size: int = int(2**20)) -> None:
+        self.stream = ZstdDecompressor(max_window_size=int(2**31)).stream_reader(
             open(path, "rb")
         )
-        self.chunk_size = chunk_size
-        self.buffer = ""
-        self.init = self.__read_chunk()
+        self.size = size
+        self.buffer = b""
+        self.lines = []
 
-    def __read_chunk(self) -> bool:
-        # * ignoring errors seems to fix bug, but may drop bytes, bug:
-        # ! UnicodeDecodeError: 'utf-8' codec can't decode byte 0xf0 in position 1023: unexpected end of data
-        self.chunk = self.reader.read(self.chunk_size).decode(errors="ignore")
-        if self.chunk:
-            self.lines = (self.buffer + self.chunk).split("\n")
-            self.buffer = self.lines[-1]
-            self.lines = self.lines[:-1]
-            return True
-        else:
-            return False
-
-    def __iter__(self) -> ZSTJSONL:
-        return self
+    def __iter__(self) -> dict:
+        while True:
+            yield next(self)
 
     def __next__(self) -> dict:
         if len(self.lines):
             return orjson.loads(self.lines.pop(0))
         else:
-            if self.__read_chunk():
+            chunk = self.stream.read(self.size)
+            if chunk:
+                self.lines = (self.buffer + chunk).split(b"\n")
+                self.buffer = self.lines[-1]
+                self.lines = self.lines[:-1]
                 return orjson.loads(self.lines.pop(0))
             else:
                 raise StopIteration()
+
+    def process(self, handler: Callable, n: int = None) -> list[Any]:
+        data = list()
+        count = 0
+        for line in tqdm(self):
+            data.append(handler(line))
+            count += 1
+            if n:
+                if count == n:
+                    break
+        return data
+
+    def ingest(self, handler: Callable, n: int = None) -> None:
+        for line in tqdm(self):
+            count = 0
+            for line in tqdm(self):
+                handler(line)
+                count += 1
+                if n:
+                    if count == n:
+                        break
