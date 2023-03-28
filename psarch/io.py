28703469d421652b1d74fcb7d97eec785b2a1be5
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-28 02:48:46 -0400

    psarch/io: refactor zst io to iterator

--- psarch/io.py
+++ psarch/io.py
@@ -1,26 +1,42 @@
+from __future__ import annotations
+
+import orjson
 from zstandard import ZstdDecompressor
 
 
-class ZSTIO:
-    "source: https://github.com/pushshift/zreader"
-
+class ZSTJSONL:
     def __init__(
-        self, file: str, chunk_size: int = int(2**10), max_window_size=int(2**31)
+        self, path: str, chunk_size: int = int(2**20), max_window_size=int(2**31)
     ) -> None:
-        self.decompressor = ZstdDecompressor(
-            max_window_size=max_window_size
-        ).stream_reader(open(file, "rb"))
+        # ! bug (hypothesis): if chunk size is less than the length of a line, bug introduced
+        # ! and cannot read
+        self.reader = ZstdDecompressor(max_window_size=max_window_size).stream_reader(
+            open(path, "rb")
+        )
         self.chunk_size = chunk_size
         self.buffer = ""
+        self.init = self.__read_chunk()
 
-    def readlines(self):
-        while True:
-            # * ignoring errors seems to fix bug, but may drop bytes, bug:
-            # ! UnicodeDecodeError: 'utf-8' codec can't decode byte 0xf0 in position 1023: unexpected end of data
-            chunk = self.decompressor.read(self.chunk_size).decode(errors="ignore")
-            if not chunk:
-                break
-            lines = (self.buffer + chunk).split("\n")
-            self.buffer = lines[-1]
-            for line in lines[:-1]:
-                yield line
+    def __read_chunk(self) -> bool:
+        # * ignoring errors seems to fix bug, but may drop bytes, bug:
+        # ! UnicodeDecodeError: 'utf-8' codec can't decode byte 0xf0 in position 1023: unexpected end of data
+        self.chunk = self.reader.read(self.chunk_size).decode(errors="ignore")
+        if self.chunk:
+            self.lines = (self.buffer + self.chunk).split("\n")
+            self.buffer = self.lines[-1]
+            self.lines = self.lines[:-1]
+            return True
+        else:
+            return False
+
+    def __iter__(self) -> ZSTJSONL:
+        return self
+
+    def __next__(self) -> dict:
+        if len(self.lines):
+            return orjson.loads(self.lines.pop(0))
+        else:
+            if self.__read_chunk():
+                return orjson.loads(self.lines.pop(0))
+            else:
+                raise StopIteration()
