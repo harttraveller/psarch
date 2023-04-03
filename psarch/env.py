8b222e28e5ff97bdf9b9d821a6ea073d7d32c4b2
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-04-03 19:08:05 -0400

    psarch/env: add version

--- psarch/env.py
+++ psarch/env.py
@@ -1,17 +1,22 @@
+from pyeio import easy
 from pathlib import Path
 
-HOME = Path.home()
-FILES = HOME / ".psarch"
-CONFIG = FILES / "config.json"
+HOME_PATH = Path.home()
+PROGRAM_FILES_PATH = HOME_PATH / ".psarch"
+CONFIG_PATH = PROGRAM_FILES_PATH / "config.json"
+CONFIG_FILE = easy.load(CONFIG_PATH)
+CACHE_PATH = CONFIG_FILE["cache"]
+
 
 # TODO: add windows support
 # TODO: add linux support
 SUPPORTED_DEVICES = {"Darwin": {"arm", "i386"}}
 
+ELASTICSEARCH_VERSION = "elasticsearch-8.7.0"
 ELASTICSEARCH_DOWNLOAD_URLS = {
     "Darwin": {
-        "i386": "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.7.0-darwin-x86_64.tar.gz",
         "arm": "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.7.0-darwin-aarch64.tar.gz",
+        "i386": "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.7.0-darwin-x86_64.tar.gz",
     }
     # "windows": "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.7.0-windows-x86_64.zip",
     # "linux_x86_64": "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.7.0-linux-x86_64.tar.gz",
